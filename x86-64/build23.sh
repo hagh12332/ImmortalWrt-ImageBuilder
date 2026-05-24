cb39ec6be196f65518bfb39aa6e4cc59ca5e5d61
hagh12332 <1710263496@qq.com>
2026-05-24 22:30:57 +0800

    Update build23.sh

--- x86-64/build23.sh
+++ x86-64/build23.sh
@@ -57,6 +57,9 @@ PACKAGES="$PACKAGES xray-core hysteria luci-i18n-passwall-zh-cn"
 PACKAGES="$PACKAGES luci-app-openclash"
 PACKAGES="$PACKAGES luci-i18n-homeproxy-zh-cn"
 PACKAGES="$PACKAGES openssh-sftp-server"
+PACKAGES="$PACKAGES luci-app-store
+PACKAGES="$PACKAGES luci-app-quickstart
+PACKAGES="$PACKAGES luci-app-adguardhome
 # ======== shell/custom-packages.sh =======
 # 合并imm仓库以外的第三方插件
 PACKAGES="$PACKAGES $CUSTOM_PACKAGES"
