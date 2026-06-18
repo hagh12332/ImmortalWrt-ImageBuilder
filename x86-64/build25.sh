5d25f13cb0c332a94eb0eab1d15f653da6e56dcd
hagh12332 <1710263496@qq.com>
2026-06-18 12:52:22 +0800

    Update build25.sh

--- x86-64/build25.sh
+++ x86-64/build25.sh
@@ -56,7 +56,7 @@ PACKAGES="$PACKAGES luci-i18n-argon-config-zh-cn"
 PACKAGES="$PACKAGES luci-i18n-package-manager-zh-cn"
 PACKAGES="$PACKAGES luci-i18n-ttyd-zh-cn"
 PACKAGES="$PACKAGES openssh-sftp-server"
-PACKAGES="$PACKAGES uci-app-openclash_0.47.071_all.ipk"
+PACKAGES="$PACKAGES uci-app-openclash"
 PACKAGES="$PACKAGES luci-app-adguardhome"
 # 文件管理器
 PACKAGES="$PACKAGES luci-i18n-filemanager-zh-cn"
